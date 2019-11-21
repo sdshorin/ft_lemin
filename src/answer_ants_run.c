f3cbd844526efb3c040e525036c6dbc687511414
Shorin Sergey <serres123@ya.ru>
2019-11-22 01:18:09 +0300

    change answer code

--- src/answer_ants_run.c
+++ src/answer_ants_run.c
@@ -5,9 +5,8 @@
 
 
 
-void make_ant(int ant_index, t_ant **first_ant, t_room *next_room)
+void make_ant(int ant_index, t_ant **first_ant, t_room *next_room, t_ant **last_ant)
 {
-	static t_ant *last_ant;
 	t_ant *now_ant;
 	
 	now_ant = (t_ant*)malloc(sizeof(t_ant));
@@ -15,38 +14,96 @@ void make_ant(int ant_index, t_ant **first_ant, t_room *next_room)
 	now_ant->now_room = next_room;
 	now_ant->next_ant = 0;
 	now_ant->to_delete = 0;
-	if (last_ant == 0)
+	if (*last_ant == 0)
 	{
-		last_ant = now_ant;
+		*last_ant = now_ant;
 		now_ant->prev_ant = 0;
 		(*first_ant) = now_ant;
 	}
 	else
 	{
-		last_ant->next_ant = now_ant;
-		now_ant->prev_ant = last_ant;
-		last_ant = now_ant;
+		(*last_ant)->next_ant = now_ant;
+		now_ant->prev_ant = *last_ant;
+		*last_ant = now_ant;
 	}
 	
 }
 
+void write_to_recipte_path_len(t_room *room)
+{
+	t_room *now_room;
+	int i;
 
+	i = 0;
+	now_room = room;
+	while (now_room)
+	{
+		i++;
+		now_room = now_room->next_on_path;
+	}
+	room->recipe.path_cost = i;
+}
 
-void create_now_ants(int *ants, int *ant_index, t_ant** first_ant, t_room *start)
+void set_all_path_len(t_data *data)
+{
+		size_t	i;
+	void **v_vector;
+	i = 0;
+	v_vector = data->start->links.data;
+	while (i < data->start->links.size)
+	{
+		write_to_recipte_path_len((t_room*)v_vector[i]);
+		i++;
+	}
+}
+
+void swap_void_vector(void **v_vector, size_t i, size_t j)
+{
+	void *temp;
+
+	temp = v_vector[i];
+	v_vector[i] = v_vector[j];
+	v_vector[j] = temp;
+}
+
+void sort_paths(t_data *data)
+{
+	size_t	i;
+	size_t	j;
+	void **v_vector;
+
+	i = 0;
+	set_all_path_len(data);
+	v_vector = data->start->links.data;
+	while (i < data->start->links.size)
+	{
+		j = i;
+		while (j < data->start->links.size)
+		{
+			if (((t_room*)v_vector[i])->recipe.path_cost > ((t_room*)v_vector[j])->recipe.path_cost)
+				swap_void_vector(v_vector, i, j);
+			j++;
+		}
+		i++;
+	}
+}
+
+// void create_now_ants(t_data *data, int *ant_index, t_ant** first_ant, t_room *start)
+void create_now_ants(t_data *data, int *ant_index, t_ant** first_ant, t_ant** last_ant)
 {
 	size_t	i;
 	void **v_vector;
-	if (*ants <= 0)
+	if (data->ants <= 0)
 		return ;
 	i = 0;
-	v_vector = start->links.data;
-	while (i < start->links.size && *ants > 0)
+	v_vector = data->start->links.data;
+	while (i < data->start->links.size && data->ants > 0)
 	{
 		if (((t_room*)v_vector[i])->path_index >= 0)
 		{
-			make_ant(*ant_index, first_ant, ((t_room*)v_vector[i]));
+			make_ant(*ant_index, first_ant, ((t_room*)v_vector[i]), last_ant);
 			(*ant_index)++;
-			(*ants)--;
+			data->ants--;
 		}
 		i++;
 	}	
@@ -57,31 +114,32 @@ t_ant *delete_ant(t_ant *ant, t_ant **first_ant)
 {
 	t_ant* to_return;
 
-	if (ant == NULL)
-		return (NULL);
+	if (*first_ant == ant)
+		*first_ant = ant->next_ant;
 	if (ant->prev_ant != NULL)
 		ant->prev_ant->next_ant = ant->next_ant;
 	to_return = ant->next_ant;
 	if (ant->next_ant != NULL)
-		ant->next_ant->prev_ant = 0;
-	if (ant == *first_ant)
-		*first_ant = to_return;
-	ft_memdel((void **)&ant);
+		ant->next_ant->prev_ant = ant->prev_ant;
+	free(ant);
+	// ft_memdel((void **)&ant);
 	return (to_return);
 }
 
-void move_now_ants(t_ant *ant, t_ant **first_ant, t_room *end)
+void move_now_ants(t_ant **first_ant) //, t_room *end)
 {
 	int need_whitespace;
+	t_ant	*ant;
 
+	ant = *first_ant;
 	need_whitespace = 0;
 	while (ant)
 	{
-		if (ant->to_delete)
-		{
-			ant = delete_ant(ant, first_ant);
-			continue ;
-		}
+		// if (ant->to_delete)
+		// {
+		// 	ant = delete_ant(ant);
+		// 	continue ;
+		// }
 		if (need_whitespace)
 			write(1, " ", 1);
 		need_whitespace = 1;
@@ -89,10 +147,13 @@ void move_now_ants(t_ant *ant, t_ant **first_ant, t_room *end)
 		ft_putnbr(ant->ant_index);
 		write(1, "-", 1);
 		ft_putstr(ant->now_room->name);
-		if (ant->now_room == end)
-			ant->to_delete = 1;
+		// if (ant->now_room == end)
+		// 	ant->to_delete = 1;
 		ant->now_room = ant->now_room->next_on_path;
-		ant = ant->next_ant;
+		if (!ant->now_room)
+			ant = delete_ant(ant, first_ant);
+		else
+			ant = ant->next_ant;
 	}
 }
 
@@ -100,14 +161,17 @@ void move_now_ants(t_ant *ant, t_ant **first_ant, t_room *end)
 void run_ants_print_answer(t_data *data)
 {
 	t_ant	*first_ant;
+	t_ant	*last_ant;
 	int		ant_index;
 	
 	first_ant = 0;
 	ant_index = 1;
+	last_ant = 0;
+	sort_paths(data);
 	while(data->ants > 0 || first_ant)
 	{
-		create_now_ants(&data->ants, &ant_index, &first_ant, data->start);
-		move_now_ants(first_ant, &first_ant, data->end);
+		create_now_ants(data, &ant_index, &first_ant, &last_ant);
+		move_now_ants(&first_ant); //, data->end);
 		if (data->ants > 0 || first_ant)
 			write(1, "\n", 1);
 	}
