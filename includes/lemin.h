b3a61d011ec125aca9a7aac304fec39e7e673339
Beef Jesse <bjesse@oa-a1.21-school.ru>
2019-11-30 17:59:11 +0300

    check if inpiut file is dir

--- includes/lemin.h
+++ includes/lemin.h
@@ -16,6 +16,8 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <string.h>
+# include <sys/types.h>
+# include <sys/stat.h>
 # include "libft.h"
 
 typedef struct	s_input
--- src/main.c
+++ src/main.c
@@ -32,22 +32,39 @@ void	lem_in_main(t_data *data)
 		run_ants_print_answer(data);
 }
 
+int		is_regular_file(const char *path)
+{
+	struct stat	path_stat;
+
+	stat(path, &path_stat);
+	return (S_ISREG(path_stat.st_mode));
+}
+
+void	read_from_file(char *path, t_data *data)
+{
+	ft_putendl("Trying to read graph from file...");
+	if (!is_regular_file(path))
+	{
+		ft_putendl("First arg isn't file!");
+		ft_putendl("Trying to read from stdin...");
+		return ;
+	}
+	data->fd = open(path, O_RDONLY);
+	if (data->fd < 0 || read(data->fd, NULL, 0) < 0)
+	{
+		ft_putendl("Cannot open file specified in first arg!");
+		ft_putendl("Trying to read from stdin...");
+		data->fd = 0;
+	}
+}
+
 int		main(int argc, char **argv)
 {
 	t_data	*data;
 
 	data = create_data_struct();
 	if (argc > 1)
-	{
-		ft_putendl("Trying to read graph from file...");
-		data->fd = open(argv[1], O_RDONLY);
-		if (data->fd < 0 || read(data->fd, NULL, 0) < 0)
-		{
-			ft_putendl("Cannot open file specified in first arg!");
-			ft_putendl("Trying to read from stdin...");
-			data->fd = 0;
-		}
-	}
+		read_from_file(argv[1], data);
 	get_data(data);
 	if (data->start == data->end)
 		error_handler("Error: start == end!", data);
